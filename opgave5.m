0a27a806eae287349ea85e446ec898a1602c1b2c
LAPTOP-J9416U0R\Casper <casper.barendrecht@gmail.com>
2017-05-19 16:48:50 +0200

    opgave 5 n foutje eruit?

--- opgave5.m
+++ opgave5.m
@@ -5,12 +5,11 @@ L = 3;
 te = 5.0;
 
 dx = L/(N+1);
-dt = 0.0001;
+dt = dx^2/(2*v);
 
 t = 0:dt:te;
 
 w = zeros(N+1,1);
-w(1) = 0;
 
 K = zeros(N+1);
 for i = 2:N
@@ -27,15 +26,16 @@ K = K/dx^2;
 
 figure;
 hold on;
-
+u = w;
 for i = 1:length(t)-1
     cla;
     w = w + dt*(v*K*w - f3(w,dx) + r3(w,v,dx));
-    
+    u = u + (1.01)*dt*(v*K*u - f3(u,dx) + r3(u,v,dx));
     subplot(2,1,2);
-    plot(dx:dx:L,w);
-    title('Verandering van u als functie van dx in de tijd');
-    axis([0 3 -2 1]);
+
+    plot(dx:dx:L,0.5-0.5*u);
+    title('Verandering van rho als functie van dx in de tijd (dt groot)');
+    axis([0 3 1/4 1]);
     
     subplot(2,1,1);
     plot(dx:dx:L,0.5-0.5*w);
